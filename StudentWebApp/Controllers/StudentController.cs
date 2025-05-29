a620129bbcba92578eaae0bf0f1e8ee0710191b7
Jaimin Mevada <jaiminkumarmevada@gmail.com>
2025-05-29 16:02:46 +0530

    Add project files.

---
+++ StudentWebApp/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using StudentWebApp.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        Db_TestEntities DBcontext = new Db_TestEntities(); 
+
+        // GET: Student
+
+        public ActionResult Student()
+        {
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public ActionResult AddStudent(tbl_student model)
+        {
+            tbl_student student = new tbl_student();
+
+            student.Fname = model.Fname;    
+            student.Email = model.Email;
+            student.Mobile = model.Mobile;
+            student.Description = model.Description;
+
+            DBcontext.tbl_student.Add(student);
+            DBcontext.SaveChanges();
+            return RedirectToAction("Student");
+
+        }
+    }
+}
